fc1d1a09b16b6d73ad0ca51f0cc44c60e5987a41
hdaof <5386787+hdaof@users.noreply.github.com>
2024-04-19 16:15:40 -0700

    update tests to account for strings

--- extra-files/01-starting-setup/basic-testing-frontend/src/math.test.js
+++ extra-files/01-starting-setup/basic-testing-frontend/src/math.test.js
@@ -4,7 +4,7 @@ import {add} from "./math";
 
 it("should summarize all number values in an array", () => {
   // Arrange
-  const numbers = [1, 2, 3];
+  const numbers = [1, 2];
 
   // Act
   const result = add(numbers);
@@ -12,4 +12,21 @@ it("should summarize all number values in an array", () => {
   // Assert
   const expectedResult = numbers.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
   expect(result).toBe(expectedResult);
+});
+
+it("should yield NaN if at least one invalid number is provided", () => {
+  const inputs = ["invalid", 2];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of contains string representations of numbers", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce((previousValue, currentValue) => +previousValue + +currentValue, 0);
+  expect(result).toBe(expectedResult)
 });
