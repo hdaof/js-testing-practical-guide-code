282bf27ca3fdd6ed955fce464c021e344abe3851
hdaof <5386787+hdaof@users.noreply.github.com>
2024-04-18 17:58:58 -0700

    build app. add test

---
+++ extra-files/01-starting-setup/basic-testing-frontend/src/math.test.js
@@ -0,0 +1,15 @@
+import {it, expect} from "vitest";
+
+import {add} from "./math";
+
+it("should summarize all number values in an array", () => {
+  // Arrange
+  const numbers = [1, 2, 3];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  const expectedResult = numbers.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+  expect(result).toBe(expectedResult);
+});
