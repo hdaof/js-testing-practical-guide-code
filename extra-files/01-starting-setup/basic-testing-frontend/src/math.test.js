55fc491cdf12cab6bd7e582679597181de7caefd
hdaof <5386787+hdaof@users.noreply.github.com>
2024-04-19 16:30:35 -0700

    additional unhappy path tests

--- extra-files/01-starting-setup/basic-testing-frontend/src/math.test.js
+++ extra-files/01-starting-setup/basic-testing-frontend/src/math.test.js
@@ -26,7 +26,42 @@ it("should yield a correct sum if an array of contains string representations of
   const numbers = ["1", "2"];
 
   const result = add(numbers);
-
   const expectedResult = numbers.reduce((previousValue, currentValue) => +previousValue + +currentValue, 0);
-  expect(result).toBe(expectedResult)
+
+  expect(result).toBe(expectedResult);
+});
+
+it("should yield 0 if empty array is provided", () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).toBe(0);
+});
+
+it("should throw an error if no value is passed into function - solution #1", () => {
+  try {
+    const result = add();
+  } catch (error) {
+    expect(error).toBeDefined();
+  }
+});
+
+it("should throw an error if no value is passed into function - solution #2", () => {
+  const resultFn = () => {
+    add();
+  };
+
+  expect(resultFn).toThrow();
+});
+
+it("should throw an error if provided with multiple arguments instead of array", () => {
+  const num1 = 1;
+  const num2 = 2;
+
+  const resultFn = () => {
+    add(num1, num2);
+  };
+
+  expect(resultFn).toThrow();
 });
