2a8c9f5ab0600511b7c63a262a9df17b459f11f1
hdaof <5386787+hdaof@users.noreply.github.com>
2024-04-19 19:17:42 -0700

    finish  tests. Add  tests

--- extra-files/01-starting-setup/basic-testing-frontend/src/util/numbers.test.js
+++ extra-files/01-starting-setup/basic-testing-frontend/src/util/numbers.test.js
@@ -1,33 +1,37 @@
 import {it, expect} from "vitest";
 import {transformToNumber} from "./numbers";
 
-it("should return an integer when a string representation of number is passed", () => {
+it("should transform a string number to a number of type number", () => {
   // Arrange
-  const number = "1";
+  const input = "1";
 
   // Act
-  const result = transformToNumber(number);
+  const result = transformToNumber(input);
 
   // Assert
   expect(result).toBe(1);
   // more tests
-  expect(result).toBe(+number);
+  expect(result).toBe(+input);
   expect(result).toBeTypeOf("number");
 });
 
-it("should return NaN if provided invalid value", () => {
-  const notNumber = "invalid";
+it("should yield NaN for non-transformable values", () => {
+  const input = "invalid";
+  const input2 = {};
 
-  const result = transformToNumber(notNumber);
+  const result = transformToNumber(input);
+  const result2 = transformToNumber(input2);
 
   expect(result).toBeNaN();
+  expect(result2).toBeNaN();
 });
 
 it.skip("should throw an error if value is already an integer", () => {
-  const number = 1;
+  //  TODO update function to throw error
+  const input = 1;
 
   const resultFn = () => {
-    transformToNumber(number);
+    transformToNumber(input);
   };
 
   expect(resultFn).toThrow();
